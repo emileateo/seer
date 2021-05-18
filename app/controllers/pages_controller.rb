d37b83bb2b3b96d77b31f4e43fe969c96c9904b1
AidenAng <aidenangdj@gmail.com>
2021-05-18 17:28:24 +0800

    post

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,16 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def studiespost
+  end
+
+  def careerpost
+  end
+
+  def relationshippost
+  end
+
+  def healthpost
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'studiespost', to: 'pages#studiespost', as: :studiespost
+  get 'careerpost', to: 'pages#careerpost', as: :careerpost
+  get 'relationshippost', to: 'pages#relationshippost', as: :relationshippost
+  get 'healthpost', to: 'pages#healthpost', as: :healthpost
 end
