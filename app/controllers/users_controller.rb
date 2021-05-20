75f38cf5ff0240111994ab067c578b2749a1000a
Emilea Teo <emileateo@gmail.com>
2021-05-20 10:32:57 +0800

    now pushing selected categories into user.categories rather than creating Preference.new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
     @preferences_array = params[:user][:preferences]
     @preferences_array.delete_at(0)
     @preferences_array.each do |category|
-      Preference.create(user: current_user, category_id: category.to_i)
+      @user.categories << Category.find(category.to_i)
     end
     redirect_to root_path
   end
