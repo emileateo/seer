799e3a44290fdcff454199c525f802d4430100c2
AidenAng <aidenangdj@gmail.com>
2021-05-21 15:02:45 +0800

    preview page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,8 @@
+require 'json'
+require 'open-uri'
+require 'zodiac'
+require 'date'
+
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :preview]
 
@@ -5,8 +10,42 @@ class PagesController < ApplicationController
   end
 
   def preview
-    @birth_date = params['date']
-    @birth_time = params['time']
+    @name = params[:name]
+    @birth_date = params[:date]
+    @year = params[:year].to_i
+
+    visitor_birth_date = Time.new(params[:date])
+    visitor_birth_date_reversed = visitor_birth_date.strftime("%d/%m/%Y")
+
+    url = "https://api.vedicastroapi.com/json/prediction/numerology?name=#{params[:name]}&show_same=true&date=#{visitor_birth_date_reversed}&type=TYPE&api_key=9ad6241b-9b66-5990-95b1-63654815da21"
+    fortune_serialized = URI.open(url).read
+    @fortune = JSON.parse(fortune_serialized)
+
+    if (@year - 2000) % 12 == 0
+        @sign = 'Dragon'
+    elsif (@year - 2000) % 12 == 1
+        @sign = 'Snake'
+    elsif (@year - 2000) % 12 == 2
+        @sign = 'Horse'
+    elsif (@year - 2000) % 12 == 3
+        @sign = 'Sheep'
+    elsif (@year - 2000) % 12 == 4
+        @sign = 'Monkey'
+    elsif (@year - 2000) % 12 == 5
+        @sign = 'Rooster'
+    elsif (@year - 2000) % 12 == 6
+        @sign = 'Dog'
+    elsif (@year - 2000) % 12 == 7
+        @sign = 'Pig'
+    elsif (@year - 2000) % 12 == 8
+        @sign = 'Rat'
+    elsif (@year - 2000) % 12 == 9
+        @sign = 'Ox'
+    elsif (@year - 2000) % 12 == 10
+        @sign = 'Tiger'
+    else
+        @sign = 'Hare'
+    end
   end
 
   def dashboard
