cb28c63e883625dc8cb621d90adb9b27e5a86877
Emilea Teo <emileateo@gmail.com>
2021-05-21 22:06:30 +0800

    passed birthdate to create user params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,22 @@
 class RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: [:create]
+
   def new
+    @birthdate = params[:birthdate]
+    super
+    # raise
+  end
+
+  def create
     @birthdate = params[:birthdate]
     super
   end
 
   protected
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:birthdate])
+  end
 
   def after_sign_up_path_for(resource)
     preferences_path
