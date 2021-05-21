245fc49396fce8d5d5aaf6cfef4fe8eb5c14ebd9
Emilea Teo <emileateo@gmail.com>
2021-05-21 21:24:32 +0800

    passed date from homepage

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,9 @@
 class RegistrationsController < Devise::RegistrationsController
+  def new
+    @birthdate = params[:birthdate]
+    super
+  end
+
   protected
 
   def after_sign_up_path_for(resource)
