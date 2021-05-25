73b1809d687d9da792b08a65c4a0c910bf215a72
AidenAng <aidenangdj@gmail.com>
2021-05-25 10:26:09 +0800

    extension

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'horoscopes/show'
+  get 'horoscopes/show', as: :extension
   # devise_for :users
   devise_for :users, controllers: { registrations: "registrations" }
 
