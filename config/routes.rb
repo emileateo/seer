67273c6cd02165a019740d0c26710fdce711ae98
Emilea Teo <emileateo@gmail.com>
2021-05-26 14:49:49 +0800

    new route, appointment_video_path, that links to videos#show

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
 
   resources :appointments, only: [:index, :update, :destroy] do
     resources :payments, only: :new
+    resources :videos, only: :show
   end
 
   get "appointments/:id", to: "appointments#really_destroy", as: :really_destroy_appointment
