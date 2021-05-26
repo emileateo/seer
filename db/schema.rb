a1c732d9e971d327a5c7bba1789948cf89a2ebfa
AidenAng <aidenangdj@gmail.com>
2021-05-26 18:21:47 +0800

    fixed merging errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_05_26_034328) do
-
+ActiveRecord::Schema.define(version: 2021_05_26_023344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,8 +91,8 @@ ActiveRecord::Schema.define(version: 2021_05_26_034328) do
     t.string "specialty"
     t.string "lucky_color"
     t.integer "lucky_number"
-    t.integer "price_cents", default: 0, null: false
     t.string "name"
+    t.integer "price_cents", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
